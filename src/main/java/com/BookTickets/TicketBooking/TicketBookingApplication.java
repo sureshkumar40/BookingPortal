c846ab2b23a91056acaff68974f419823dcbc5f6
sureshkumar40 <suresh_kb08@yahoo.com>
2018-01-08 12:53:31 +0530

    commit

--- src/main/java/com/BookTickets/TicketBooking/TicketBookingApplication.java
+++ src/main/java/com/BookTickets/TicketBooking/TicketBookingApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication(scanBasePackages = { "com.BookTickets.TicketBooking", "com.BookTickets.TicketBooking.Controller", "com.BookTickets.TicketBooking.Domain", "com.BookTickets.TicketBooking.Repository" })
-@EnableMongoRepositories({"com.BookTickets.TicketBooking.Repository"})
+//@EnableMongoRepositories({"com.BookTickets.TicketBooking.Repository"})
 public class TicketBookingApplication {
 
 	public static void main(String[] args) {
